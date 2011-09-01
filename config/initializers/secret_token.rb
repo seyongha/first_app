8d8a920ef11759194a2df050dda34f13c1b9a96c
Bubble pop <seyong.ha@facebook.com>
2011-09-02 01:56:06 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cbaabe332830dee6ccebd4dd5c08add9c09c0b855b0966b6014e94c77d410361c9bd4d3576c5b91bdaa10a5adb06f04760382625177f4adaa5f92138de66f8ca'
